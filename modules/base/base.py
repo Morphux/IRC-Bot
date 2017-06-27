803fefa3d7bd802eee014590226f4a5e25a0ed05
root <root@163-172-86-74.online.net>
2017-06-27 16:34:42 +0200

    Stash(Base.py): DO NOT COMMIT THIS

--- modules/base/base.py
+++ modules/base/base.py
@@ -2,6 +2,7 @@
 # By: Louis <louis@ne02ptzero>
 
 import random
+import socket
 
 class Base:
 
@@ -9,13 +10,27 @@ class Base:
         Morphux.sendMessage("Pong !", infos['nick'])
 
     def join(self, Morphux, user):
-        Morphux.sendMessage("Hey, howr u doin :) ?", user)
+        if (user != "RMS"):
+            Morphux.sendMessage("Hi there !", user)
 
     def nyan(self, Morphux, infos):
         Morphux.sendMessage(str("telnet nyancat.dakko.us"), infos['nick'])
 
     def ah(self, Morphux, infos):
         Morphux.sendMessage("https://youtu.be/XE6YaLtctcI", infos['nick'])
+
+    def rms(self, Morphux, line):
+        infos = Morphux.getInfo(line, 1)
+        if (infos == False):
+            return 1;
+        if (infos['nick'] == "RMS"):
+            return 1
+        if (("Linux" in line or "linux" in line) and "GNU" not in line):
+            clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            clientsocket.connect(('localhost', 1337))
+            cmd = 'main ' + infos['nick']
+            clientsocket.send(cmd.encode())
+
 
     def command(self):
         self.config = {
@@ -38,6 +53,9 @@ class Base:
             },
             "onJoin": {
                 "join": self.join
+            },
+            "before": {
+                "rms": self.rms
             }
         }
         return self.config
