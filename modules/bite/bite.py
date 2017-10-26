0c9c1fc1f3124f4687ac200e65c6cc582741adb0
Jules Hézard <hezardj@gmail.com>
2017-10-26 13:02:27 +0200

    add module bite (#14)
    * add module bite
    
    * Alignment

---
+++ modules/bite/bite.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Bite Module
+# By: Enerdhil <hezardj@gmail.com>
+
+class Bite:
+
+    def command(self):
+        self.config = {
+                "command": {
+                        "bite": {
+                                "function": self.bite,
+                                "usage": "bite",
+                                "help": "C'est ma bite !"
+                        },
+                        "mabite": {
+                                "function": self.mabite,
+                                "usage": "bite",
+                                "help": "C'est ma bite !"
+                        },
+                }
+        }
+        return self.config
+
+    def bite(self, Morphux, infos):
+        Morphux.sendMessage("c∷∷∷∷∷3", infos['nick'])
+
+    def mabite(self, Morphux, infos):
+        Morphux.sendMessage("∷∷∷∷∷Э")
+        Morphux.sendMessage("OO")
